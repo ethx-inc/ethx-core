8f51d149b6501e782aa858c08c5e247ea32f2097
Powell, Anthony <powell.anthony.e@gmail.com>
2021-03-13 00:06:20 -0600

    adding prettierrc file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const navy = "Navy";
+const navy = `Navy`;
 
 export const Home = () => {
   return (
     <div className="mx-auto w-3/5">
       <header></header>
-      <div className='font-bold text-7xl text-center'>Welcome To ETHX!</div>
+      <div className="font-bold text-7xl text-center">Welcome To ETHX!</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default Home;
