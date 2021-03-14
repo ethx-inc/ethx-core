320dbe13e93ed2bdcd5a3ee41b2727d089ec9684
Powell, Anthony <powell.anthony.e@gmail.com>
2021-03-13 23:05:43 -0600

    flushed out eslint and added husky for git hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
-const navy = `Navy`;
+const navy = ' Navy ';
 
-export const Home = () => {
-  return (
-    <div className="mx-auto w-3/5">
-      <header></header>
-      <div className="font-bold text-7xl text-center">Welcome To ETHX!</div>
-    </div>
-  );
+console.log(navy);
+
+export const Home = (killer: string): JSX.Element => {
+	console.log(killer);
+
+	return (
+		<div className='mx-auto w-3/5'>
+			<header />
+			<div className='font-bold text-7xl text-center'>
+				Welcome To ETHX!
+			</div>
+		</div>
+	);
 };
 
 export default Home;
