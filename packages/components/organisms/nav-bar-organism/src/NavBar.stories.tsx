2eb9b6e6f78cd1ec63686a5adab368d4658980f5
Kristian Martinez <kmartman94@gmail.com>
2021-03-24 20:35:32 -0700

    fix(nav-bar): was atom when should have been organism

--- packages/components/organisms/nav-bar-organism/src/NavBar.stories.tsx
+++ packages/components/organisms/nav-bar-organism/src/NavBar.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
 
-import { NavBar, NavBarProps } from './NavBar.atom';
+import { NavBar, NavBarProps } from './NavBar.organism';
 
 // eslint-disable-next-line import/no-unresolved
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
