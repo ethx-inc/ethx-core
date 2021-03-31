d518accb8086bcb2306fafd197fb60c121bed41b
dahlia15 <dahliaamade1@gmail.com>
2021-03-31 15:00:15 -0700

    build:ethx filter atoms

---
+++ packages/components/atoms/icon-image/src/IconImage.atom.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../../../../../styles/globals.css';
+
+export interface IconImageProps {
+	// iconUrl: string;
+	borderColor: string;
+}
+
+export const IconImage = ({ borderColor }: IconImageProps): JSX.Element => {
+
+	return (
+		<div className={`rounded-full object-contain border-2 border-${borderColor} w-24 h-24`}>
+			{/* <img src={`${iconUrl}`} alt='rice-icon-png' width={24} height={24}/> */}
+		</div>
+	);
+};
