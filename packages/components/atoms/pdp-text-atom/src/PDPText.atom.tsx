116ccb54a848dc7afd64350e071a73999b918819
dahlia15 <dahliaamade1@gmail.com>
2021-04-15 11:43:54 -0700

    feat(pdp): add price, size, brand name, etc.

--- packages/components/atoms/pdp-text-atom/src/PDPText.atom.tsx
+++ packages/components/atoms/pdp-text-atom/src/PDPText.atom.tsx
@@ -20,13 +20,16 @@ export const PDPText = ({
 	return (
 		<div
 			className={`text-${fontColor} relative lg:w-2/5 mx-5 mt-5 lg:mt-0`}>
-			<h3 className={`text-lg mb-2`}>{productName}</h3>
+			<p className={`font-light`}>The Most Ethical Brand</p>
+			<h3 className={`text-lg font-bold mb-2`}>{productName}</h3>
 			<ul className={`list-disc text-sm mb-4 ml-10`}>
 				<li>{bulletOne}</li>
 				<li>{bulletTwo}</li>
 				<li>{bulletThree}</li>
 			</ul>
-			<p>{productDetails}</p>
+			<p className={`text-sm`}>{productDetails}</p>
+			<p className={`font-light my-3`}>Size: 0.67 fl oz / 20 mL </p>
+			<p className={`font-bold my-5`}>$40 USD</p>
 		</div>
 	);
 };
