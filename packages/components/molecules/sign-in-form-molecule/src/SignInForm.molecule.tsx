fce0879c6b79fef73f5a43f8d8a9dcf9685202cf
dahlia15 <dahliaamade1@gmail.com>
2021-05-12 14:21:10 -0700

    fix(signin/signup): updated design to align with overall look and feel

--- packages/components/molecules/sign-in-form-molecule/src/SignInForm.molecule.tsx
+++ packages/components/molecules/sign-in-form-molecule/src/SignInForm.molecule.tsx
@@ -25,12 +25,23 @@ export const SignInForm: FC<SignInFormProps> = ({
 	return (
 		<form
 			onSubmit={event => onSubmit(event, { email, password })}
-			className='flex flex-col h-80 justify-between items-center max-w-2xl w-full'>
-			<SectionText title='sign in' />
+			className='flex flex-col h-96 justify-between max-w-2xl w-full lg:border-2 lg:rounded-lg px-8 lg:bg-white'>
+			<SectionText title='Sign In' />
+			<NavButton
+				label='Continue with Google'
+				bgColor='primary'
+				color='white'
+				css='mx-auto my-3 lg:my-0 lg:mb-2'
+				onClick={event => onClickGoogle(event)}>
+				<FcGoogle className='mx-2' />
+			</NavButton>
+			<div className='flex justify-center text-primary'>
+				<div className='w-10 border-t-2 border-primary mt-3 mx-2'></div><h4>or</h4><div className='w-10 border-t-2 border-primary mt-3 mx-2'></div>
+			</div>
 			<TextInput
 				placeholder='email'
 				type='email'
-				css='mb-3'
+				css='my-3'
 				onChange={event =>
 					setLoginData({ ...loginData, email: event.target.value })
 				}
@@ -43,16 +54,12 @@ export const SignInForm: FC<SignInFormProps> = ({
 				onChange={event => setPassword(event.target.value)}
 				required
 			/>
-			<SubmitButton title='sign in' css='mb-2' />
-			<NavButton
-				label='sign in'
-				bgColor='primary'
-				color='white'
-				css='mb-2'
-				onClick={event => onClickGoogle(event)}>
-				<FcGoogle className='md:mt-1.5 mt-1 mr-1 text-lg' />
-			</NavButton>
-			<NavLink label='sign up' href='signup' />
+
+			<div className='flex flex-row justify-between items-center mb-4'>
+				<h4 className='underline text-sm text-gray-500 ml-2 lg:-mt-5'>Forgot Password?</h4>
+				<SubmitButton title='Sign In' css='w-48' />
+			</div>
+			<div className='flex flex-row justify-center border-t-2 pt-5 mb-8'><h4 className='text-gray-500'>Don't have an account?</h4><NavLink label='Sign Up' href='signup' css='underline -ml-2'/></div>
 		</form>
 	);
 };
--- packages/components/molecules/sign-up-form-molecule/src/SignUpForm.molecule.tsx
+++ packages/components/molecules/sign-up-form-molecule/src/SignUpForm.molecule.tsx
@@ -35,26 +35,39 @@ export const SignUpForm: FC<SignUpFormProps> = ({
 					confirmPassword,
 				})
 			}
-			className='flex flex-col h-3/5 justify-between items-center max-w-2xl w-full '>
-			<SectionText title='sign up' />
-			<TextInput
-				placeholder='first name'
-				css='mb-3'
-				onChange={event =>
-					setLoginData({ ...loginData, fname: event.target.value })
-				}
-				required
-				value={fname}
-			/>
-			<TextInput
-				placeholder='last name'
-				css='mb-3'
-				onChange={event =>
-					setLoginData({ ...loginData, lname: event.target.value })
-				}
-				required
-				value={lname}
-			/>
+			className='flex flex-col h-96 justify-between max-w-2xl w-full lg:border-2 lg:rounded-lg px-8 lg:bg-white'>
+			<SectionText title='Sign Up' />
+			<NavButton
+				label='Continue with Google'
+				bgColor='primary'
+				color='white'
+				css='mx-auto my-3 lg:my-0 lg:mb-2'
+				onClick={event => onClickGoogle(event)}>
+				<FcGoogle className='mx-2' />
+			</NavButton>
+			<div className='flex justify-center text-primary'>
+				<div className='w-10 border-t-2 border-primary my-3 mx-2'></div><h4>or</h4><div className='w-10 border-t-2 border-primary mt-3 mx-2'></div>
+			</div>
+			<div className='flex flex-row justify-center my-3'>
+				<TextInput
+					placeholder='first name'
+					css='mr-2'
+					onChange={event =>
+						setLoginData({ ...loginData, fname: event.target.value })
+					}
+					required
+					value={fname}
+				/>
+				<TextInput
+					placeholder='last name'
+					css=''
+					onChange={event =>
+						setLoginData({ ...loginData, lname: event.target.value })
+					}
+					required
+					value={lname}
+				/>
+			</div>
 			<TextInput
 				placeholder='email'
 				type='email'
@@ -65,10 +78,11 @@ export const SignUpForm: FC<SignUpFormProps> = ({
 				required
 				value={email}
 			/>
+			<div className='flex flex-row justify-center mb-3'>
 			<TextInput
 				placeholder='password'
 				type='password'
-				css='mb-3'
+				css='mr-2'
 				onChange={event => setPassword(event.target.value)}
 				required
 				value={password}
@@ -76,21 +90,16 @@ export const SignUpForm: FC<SignUpFormProps> = ({
 			<TextInput
 				placeholder='confirm password'
 				type='password'
-				css='mb-3'
+				css=''
 				onChange={event => setConfirmPassword(event.target.value)}
 				required
 				value={confirmPassword}
 			/>
-			<SubmitButton title='sign up' css='mb-3' />
-			<NavButton
-				label='sign up'
-				bgColor='primary'
-				color='white'
-				css='mb-2'
-				onClick={event => onClickGoogle(event)}>
-				<FcGoogle className='md:mt-1.5 mt-1 mr-1 text-lg' />
-			</NavButton>
-			<NavLink label='sign in' href='signin' />
+			</div>
+			<div className='flex flex-row justify-between mb-10 mt-3'>
+				<div className='flex flex-col lg:flex-row mb-2 lg:-mt-4 ml-2'><h3 className='text-gray-500 text-sm'>Already have an account?</h3><NavLink label='Sign In' href='signin' css='underline text-sm -ml-4 lg:-ml-2' /></div>
+				<SubmitButton title='Sign Up' css='w-48 mb-3' />
+			</div>
 		</form>
 	);
 };
