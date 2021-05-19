a6a19e45744b10b5a06f27771e42c9888209a739
dahlia15 <dahliaamade1@gmail.com>
2021-05-19 10:59:05 -0700

    fix(home): remove for loop

--- packages/components/atoms/more-filter-atom/src/MoreFilter.atom.tsx
+++ packages/components/atoms/more-filter-atom/src/MoreFilter.atom.tsx
@@ -13,15 +13,8 @@ export const MoreFilter = ({
 	onClick,
 }: MoreFilterProps): JSX.Element => {
 
-	const sampleData: { text: string; id: number }[] = [];
-	for (let index = 0; index < 7; index += 1) {
-		sampleData[0] = { text: 'Makeup', id: index[0] };
-		sampleData[1] = { text: 'Hair Care', id: index[1] };
-		sampleData[2] = { text: 'Skin Care', id: index[2] };
-		sampleData[3] = { text: 'Self Care', id: index[3] };
-		sampleData[4] = { text: 'Sun Care', id: index[4] };
+	const byCategory: string[] = ['Makeup', 'Hair Care', 'Skin Care', 'Sun Care', 'Personal Care', 'Bath', 'Fragrance' ];
 
-	}
 	return (
 		<div
 			className={`h-10 text-md overflow-scroll lg:text-lg border-2 text-${fontColor} border-primary flex justify-between rounded-full m-auto px-5 py-1 w-full lg:w-3/5 
@@ -29,11 +22,11 @@ export const MoreFilter = ({
 			${fontColor ? `text-${fontColor}` : 'text-gray-500'} `}
 			style={{ fontFamily: 'Roboto' }}>
 			<ul className=''>
-				{sampleData.map(item => (
-					<li key={item.id} 
+				{byCategory.map((category) => (
+					<li
 					className={`text-${fontColor} cursor-pointer`}
 					onClick={() => onClick()}>
-						{item.text}
+						{category}
 					</li>
 				))}
 			</ul>
