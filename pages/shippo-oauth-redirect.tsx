d3ebfaf1cd109c67e50773a604c9c4c0c392eb11
kervin philippe <55342854+kervdev21@users.noreply.github.com>
2021-06-07 15:48:13 -0400

    fix: duplication error

--- pages/shippo-oauth-redirect.tsx
+++ pages/shippo-oauth-redirect.tsx
@@ -22,20 +22,4 @@ const ShippoOnboardingSuccess: FC<ShippoOnboardingProps> = ({
 }
 
 export default ShippoOnboardingSuccess;
-=======
 
-import { HeroText } from '../packages/components/atoms/hero-text/src';
-import { NavBarController } from '../packages/controllers/navbar-controller/NavBarController';
-
-const ShippoOnboardingSuccess: FC = () => (
-	<div>
-		<NavBarController />
-		<HeroText
-			fontColor='primary'
-			subtitle='Launching Summer 2021'
-			title='Onboarding successful!'
-		/>
-	</div>
-);
-
-export default ShippoOnboardingSuccess;
